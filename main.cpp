f13c4f7a676cd775a944cac54b680f3aedab37a0
UVentura <ualivent@gmail.com>
2022-07-31 10:43:06 -0300

    Adding GLFW/Glad Example

--- main.cpp
+++ main.cpp
@@ -1,6 +1,38 @@
-#include <iostream>
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
 
-int main()
+int main( void )
 {
-    std::cout << "Hello World!\n";
+    GLFWwindow* window;
+
+    /* Init GLFW */
+    if(!glfwInit())
+        return -1;
+
+    window = glfwCreateWindow(640, 480, "OpenGL Studies", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    // Loading Context
+    glfwMakeContextCurrent(window);
+    gladLoadGL(glfwGetProcAddress);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        // Clear Screen
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.03f,0.0f,0.1f,0.03f);
+
+        /* Swap buffers */
+        glfwSwapBuffers(window);
+
+        // Get Input Events
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
